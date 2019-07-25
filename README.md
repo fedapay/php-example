fbe5d79b2aaa6f4c004ee645dd13b964d5dd7f47
Boris Koumondji <brexis2009@yahoo.fr>
2019-07-25 18:52:46 +0000

    Add comment and redme

---
+++ README.md
@@ -0,0 +1,3 @@
+## FedaPay PHP Example
+
+This project contains code samples of FedaPay php integration. Feel free to fork and contribute.
--- index.php
+++ index.php
@@ -8,6 +8,9 @@ use \FedaPay\Transaction;
 FedaPay::setApiKey("sk_sandbox_QOnQj0rYVtoDb-jOMVurIz60");
 FedaPay::setEnvironment('sandbox');
 
+/**
+ * Create a transaction
+ */
 $transaction = Transaction::create([
   "description" => "Transaction for john.doe@example.com",
   "amount" => 1,
@@ -18,4 +21,16 @@ $transaction = Transaction::create([
       "email" => "john.doe@example.com"
   ]
 ]);
+
+/**
+ * Send a mobile money request
+ */
 $transaction->sendNow('mtn', ['phone_number' => ['number' => '66994148', "country" => "bj"]]);
+
+/**
+ * Or generate a payment link to let the customer choose his payment mode
+ *
+ * $token = $transaction->generateToken();
+ * // to paymen page at $token->url
+ */
+ 
